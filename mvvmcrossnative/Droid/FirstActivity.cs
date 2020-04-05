72529117b8b086891d7c9c4f761c927babda97f1
Preetanshu Mishra <e9002102@ltfinc.net>
2020-04-05 11:23:11 +0530

    Initial Project commit. ADding MvvmCross setup after this.

---
+++ mvvmcrossnative/Droid/FirstActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace mvvmccrossnative.Droid
+{
+    [Activity(Label = "mvvmccrossnative", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class FirstActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
---
+++ mvvmcrossnative/iOS/FirstViewController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UIKit;
+
+namespace mvvmccrossnative.iOS
+{
+    public class FirstViewController : UIViewController
+    {
+        public FirstViewController()
+        {
+        }
+    }
+}
