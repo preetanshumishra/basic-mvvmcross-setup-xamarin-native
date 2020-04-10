1c964b5c8261dd61565477b11e61b12fc49da792
Preetanshu Mishra <>
2020-04-10 05:58:04 +0530

    Pushing solution, removing errors.

---
+++ mvvmcrossnative/Droid/AndroidApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using mvvmcrossnative.Core;
+
+namespace mvvmccrossnative.Droid
+{
+    [Application]
+    public class AndroidApplication : MvxAppCompatApplication<AndroidSetup, App>
+    {
+        public AndroidApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+
+        }
+    }
+}
---
+++ mvvmcrossnative/iOS/IosSetup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MvvmCross.Platforms.Ios.Core;
+using MvvmCross.ViewModels;
+using mvvmcrossnative.Core;
+
+namespace mvvmccrossnative.iOS
+{
+    public class IosSetup : MvxIosSetup<App>
+    {
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}
